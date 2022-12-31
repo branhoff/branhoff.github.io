23a39675fe0a58b2066f1ebc0e58e51cd0870473
Brandon Hoffman <branhoff2@gmail.com>
2022-12-31 03:49:46 -0800

    added feature so that when hovering over a theme button text will change to display theme-dot attribute data-text

--- themeTextGenerator.js
+++ themeTextGenerator.js
@@ -1,11 +1,26 @@
 const dots = document.querySelectorAll('.theme-dot');
 const themeText = document.getElementById('theme-text');
 
+const currentTheme = localStorage.getItem('currentTheme');
+
+// Find the current theme dot
+const currentThemeDot = Array.from(dots).find(dot => dot.getAttribute('data-mode') === currentTheme);
+
+if (currentThemeDot) {
+  // Set the text of the theme text element to the current theme text
+  themeText.innerHTML = currentThemeDot.getAttribute('data-text');
+}
+
 dots.forEach(dot => {
-  dot.addEventListener('mouseenter', () => {
-    themeText.innerHTML = 'new text';
-  });
-  dot.addEventListener('mouseleave', () => {
-    themeText.innerHTML = '';
-  });
+    dot.addEventListener('mouseenter', () => {
+        const text = dot.getAttribute('data-text');
+        themeText.innerHTML = text;
+    });
+    
+    dot.addEventListener('mouseleave', () => {
+        // Only reset the text if it is not already the text of the hovered dot
+        if (themeText.innerHTML !== dot.getAttribute('data-text')) {
+          themeText.innerHTML = currentThemeDot.getAttribute('data-text');
+        }
+    });
 });
